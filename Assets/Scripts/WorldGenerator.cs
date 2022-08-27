6867021c94808e4b2692c3f35abdf6a185521a69
Ben Clothier <ben.clothier8@icloud.com>
2022-08-27 17:01:51 +0100

    Add back grid

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -3,10 +3,13 @@ using Game.Utility.Networking;
 
 public class WorldGenerator : NetworkSingleton<WorldGenerator>
 {
-    [SerializeField] private Grid grid;
+    [SerializeField] private Grid[] grids;
     [SerializeField] private GameObject cell;
+
     [SerializeField] private Vector2Int gridFillMin;
     [SerializeField] private Vector2Int gridFillMax;
+    [SerializeField] private Vector2Int backGridFillMin;
+    [SerializeField] private Vector2Int backGridFillMax;
 
     public override void OnNetworkSpawn()
     {
@@ -18,7 +21,15 @@ public class WorldGenerator : NetworkSingleton<WorldGenerator>
             {
                 for (int y = gridFillMin.y; y <= gridFillMax.y; y++)
                 {
-                    Instantiate(cell, grid.CellToWorld(new Vector3Int(x, y, 0)), Quaternion.identity, grid.transform);
+                    Instantiate(cell, grids[0].CellToWorld(new Vector3Int(x, y, 0)), Quaternion.identity, grids[0].transform);
+                }
+            }
+
+            for (int x = backGridFillMin.x; x <= backGridFillMax.x; x++)
+            {
+                for (int y = backGridFillMin.y; y <= backGridFillMax.y; y++)
+                {
+                    Instantiate(cell, grids[1].CellToWorld(new Vector3Int(x, y, 0)), Quaternion.identity, grids[1].transform);
                 }
             }
         }
