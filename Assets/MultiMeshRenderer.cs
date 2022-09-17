7d009e8a9c14d48cc494f031935dedd401d99827
Ben Clothier <ben.clothier8@icloud.com>
2022-09-17 12:51:30 +0100

    Improve outline shader and pickup prefabs with multimeshrenderer

--- Assets/MultiMeshRenderer.cs
+++ Assets/MultiMeshRenderer.cs
@@ -6,28 +6,18 @@ public class MultiMeshRenderer : MonoBehaviour
 {
     [SerializeField] private Material[] materials;
 
+    private MeshFilter meshFilter;
+
+    private void OnEnable()
+    {
+        meshFilter = gameObject.GetComponent<MeshFilter>();
+    }
+
     private void LateUpdate()
     {
         foreach (Material material in materials)
         {
-            DrawAllMeshes(gameObject, material);
-        }
-    }
-
-    public static void DrawAllMeshes(GameObject gameObject, Material material)
-    {
-        MeshFilter[] meshFilters = gameObject.GetComponentsInChildren<MeshFilter>();
-
-        foreach (var meshFilter in meshFilters)
-        {
-            // Static objects may use static batching, preventing us from accessing their default mesh
-            if (!meshFilter.gameObject.isStatic)
-            {
-                var mesh = meshFilter.sharedMesh;
-                // Render all submeshes
-                for (int i = 0; i < mesh.subMeshCount; i++)
-                    Graphics.DrawMesh(mesh, gameObject.transform.position, Quaternion.identity, material, 0);
-            }
+            Graphics.DrawMesh(meshFilter.sharedMesh, gameObject.transform.position, gameObject.transform.rotation, material, 0);
         }
     }
 }
